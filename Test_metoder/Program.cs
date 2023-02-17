9f76614a641e0ae836efd8b1da8eb8f23326c940
Caspian2 <Admin@TEB127025>
2023-02-17 10:25:39 +0100

    Add project files.

---
+++ Test_metoder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test_metoder1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal (bredd)");
+            int bredd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in ett heltal till (höjd)");
+            int höjd = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Arean på rektangeln är :" + AREA(bredd,höjd));
+
+
+        }
+
+        static int AREA(int bredd, int höjd)
+        {
+            int a = bredd * höjd;
+
+            return a;
+        }
+    }
+}
